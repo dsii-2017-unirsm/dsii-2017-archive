8db395ef310d8f3f4e494806e40b9aa633eb890b
morenaugulini <morena.ugulini@unirsm.sm>
2017-06-04 19:55:58 +0200

    Create config.js

---
+++ morenaugulini/Making-Visible/Codici/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         'C3NLp8BPex6yBbYjONMcS4ZTe'
+  , consumer_secret:      'd5YF06AMLkKycrjA61Jr6kDTYVg8iuEueuB3yzViCSx2e2OYmq'
+  , access_token:         '856874673652596736-xKxkj6f4UT1y986aoZQ0i7CXa5aEFc1'
+  , access_token_secret:  '44n84kfld5IeyhgaKc5HaY5KraCZqRbtdwNd92HnEy0UD'
+
+}
